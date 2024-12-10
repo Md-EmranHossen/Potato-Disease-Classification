2b958d0fa0bf32a5f2da3b166637ca759aa131c4
RidwanSupon <ridwansupon@gmail.com>
2024-12-10 17:21:28 +0600

    image type add bmp

--- Src/Potato-Disease/app.py
+++ Src/Potato-Disease/app.py
@@ -63,7 +63,7 @@ def home():
 
 # Function to check if the file has an allowed extension
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg','bmp'}
 
 if __name__ == '__main__':
     app.run(debug=True)
